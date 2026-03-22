2f4d0163a68015be6792d1cdc86f974ef9749937
Tanvi <bhosaletanvi59@gmail.com>
2026-03-22 17:19:33 +0530

    file 3 added

--- single_calls/p3.java
+++ single_calls/p3.java
@@ -1,3 +1,18 @@
-public class p3 {
-    
+// N to 1 without using count
+import java.util.*;
+class p3{
+    public static void main(String [] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter Number : ");
+        int n=sc.nextInt();
+        fun(n);
+    }
+    static void fun(int n){
+        if(n<1){
+            return;
+        }
+        System.out.println(n);
+        
+        fun(n-1);
+    }
 }
